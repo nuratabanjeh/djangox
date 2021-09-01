d3f01481fd75d909fa8e42f2b1da98518f0a589a
nuratabanjeh <nuratabanjeh96@gmail.com>
2021-09-01 19:19:36 +0300

    tests

--- djangoX/tests.py
+++ djangoX/tests.py
@@ -1,3 +1,65 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Snack
+
+
+class SnackTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="tester", email="tester@email.com", password="pass"
+        )
+
+        self.snack = Snack.objects.create(
+            name="pickle", description=1, purchaser=self.user,
+        )
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.snack), "pickle")
+
+    def test_snack_content(self):
+        self.assertEqual(f"{self.snack.name}", "pickle")
+        self.assertEqual(f"{self.snack.purchaser}", "tester@email.com")
+        self.assertEqual(self.snack.description, 1)
+
+    def test_snack_list_view(self):
+        response = self.client.get(reverse("snack_list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "pickle")
+        self.assertTemplateUsed(response, "snack_list.html")
+
+    def test_snack_detail_view(self):
+        response = self.client.get(reverse("snack_detail", args="1"))
+        no_response = self.client.get("/100000/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, "purchaser: tester@email.com")
+        self.assertTemplateUsed(response, "snack_detail.html")
+
+    def test_snack_create_view(self):
+        response = self.client.post(
+            reverse("snack_create"),
+            {
+                "name": "Rake",
+                "description": "2",
+                "purchaser": self.user.id,
+            }, follow=True
+        )
+
+        self.assertRedirects(response, reverse("snack_detail", args="2"))
+        self.assertContains(response, "Details about Rake")
+
+
+
+    def test_snack_update_view_redirect(self):
+        response = self.client.post(
+            reverse("snack_update", args="1"),
+            {"name": "Updated name","description":"3","purchaser":self.user.id}
+        )
+
+        self.assertRedirects(response, reverse("snack_detail", args="1"))
+
+    def test_snack_delete_view(self):
+        response = self.client.get(reverse("snack_delete", args="1"))
+        self.assertEqual(response.status_code, 200)
--- djangoX/views.py
+++ djangoX/views.py
@@ -5,25 +5,25 @@ from .models import Snack
 from django.shortcuts import render
 
 class SnackListView(ListView):
-    template_name = "snack_pages/snack_list.html"
+    template_name = "snack_list.html"
     model = Snack
 
 class SnackDetailView(DetailView):
-    template_name = "snack_pages/snack_detail.html"
+    template_name = "snack_detail.html"
     model = Snack
 
 class SnackUpdateView(UpdateView):
-    template_name = "snack_pages/snack_update.html"
+    template_name = "snack_update.html"
     model = Snack
     fields = ["name", "purchaser", "description"]
 
 
 class SnackCreateView(CreateView):
-    template_name = "snack_pages/snack_create.html"
+    template_name = "snack_create.html"
     model = Snack
     fields = ["name", "purchaser", "description"]
 
 class SnackDeleteView(DeleteView):
-    template_name = "snack_pages/snack_delete.html"
+    template_name = "snack_delete.html"
     model = Snack
     success_url = reverse_lazy("snack_list")
